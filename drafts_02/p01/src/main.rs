a983685ab3ccd6ff7137170e2aeda37e0d0b0a1a
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-07-01 19:40:27 -0300

    Draft updated

--- drafts_02/p01/src/main.rs
+++ drafts_02/p01/src/main.rs
@@ -1,3 +1,39 @@
+struct User {
+    username: String,
+    email: String,
+}
+
+impl User {
+    pub fn exists(&self, target_email: &str, users: &mut Vec<String>) -> Result<bool, String> {
+        for email in users.iter() {
+            if *email == target_email.to_string() {
+                return Ok(true);
+            }
+        }
+        return Err(String::from("User not found"))
+    }
+    
+    pub fn login(&self, password: &str) -> Result<bool, String>{
+        Ok(true)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut users: Vec<String> = vec![String::from("guilherme@mail.com")];
+
+    let me:User = User {
+        username: String::from("Guilherme Sampaio"),
+        email: String::from("guilherme@mail.com")
+    };
+
+    match me.exists(&me.email, &mut users) {
+        Ok(value) => {
+            if value {
+                println!("User found in vector!");
+            } else {
+                println!("User not found in vector!");
+            }
+        }
+        Err(err) => println!("User not found in vector: {}", err),
+    }
 }
